b73a4f487f5e3da067670843fedb46f845daaed8
ilia <iliiliusiki@gmail.com>
2025-04-29 12:26:37 +0500

    refactor setup

--- dc_motor_driver.ino
+++ dc_motor_driver.ino
@@ -1,30 +1,32 @@
 #include <AFMotor.h>     // подключаем библиотеку для шилда
+#define MAX_SPEED 255
+
 AF_DCMotor motorBackLeft(1); // подключаем мотор к клеммникам M1
 AF_DCMotor motorBackRight(2); // подключаем мотор к клеммникам M2
 AF_DCMotor motorFrontLeft(3); // подключаем мотор к клеммникам M3
 AF_DCMotor motorFrontRight(4); // подключаем мотор к клеммникам M4
 
+void runMotor(AF_DCMotor motor, int speed, int direction) {
+  motor.setSpeed(speed); // задаем максимальную скорость мотора
+  motor.run(direction);   // останавливаем мотор
+}
 
 void setup() {
-  motorBackLeft.setSpeed(255); // задаем максимальную скорость мотора
-  motorBackLeft.run(RELEASE);   // останавливаем мотор
-  motorBackRight.setSpeed(255); // задаем максимальную скорость мотора
-  motorBackRight.run(RELEASE);   // останавливаем мотор
-  motorFrontLeft.setSpeed(255); // задаем максимальную скорость мотора
-  motorFrontLeft.run(RELEASE);   // останавливаем мотор
-  motorFrontRight.setSpeed(255); // задаем максимальную скорость мотора
-  motorFrontRight.run(RELEASE);   // останавливаем мотор
+  runMotor(motorBackLeft, MAX_SPEED, RELEASE);
+  runMotor(motorBackRight, MAX_SPEED, RELEASE);
+  runMotor(motorFrontLeft, MAX_SPEED, RELEASE);
+  runMotor(motorFrontRight, MAX_SPEED, RELEASE);
 }
 
 void loop() {
   motorBackLeft.run(FORWARD);  // задаем движение вперед
   motorBackRight.run(FORWARD);  // задаем движение вперед
-  motorBackLeft.setSpeed(255);   // задаем скорость движения
-  motorBackRight.setSpeed(255);   // задаем скорость движения
+  motorBackLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
+  motorBackRight.setSpeed(MAX_SPEED);   // задаем скорость движения
   motorFrontLeft.run(FORWARD);  // задаем движение вперед
   motorFrontRight.run(FORWARD);  // задаем движение вперед
-  motorFrontLeft.setSpeed(255);   // задаем скорость движения
-  motorFrontRight.setSpeed(255);   // задаем скорость движения
+  motorFrontLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
+  motorFrontRight.setSpeed(MAX_SPEED);   // задаем скорость движения
 
 
 
@@ -39,12 +41,12 @@ void loop() {
 
   motorBackLeft.run(BACKWARD); // задаем движение назад
   motorBackRight.run(BACKWARD); // задаем движение назад
-  motorBackLeft.setSpeed(255);   // задаем скорость движения
-  motorBackRight.setSpeed(255);   // задаем скорость движения
+  motorBackLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
+  motorBackRight.setSpeed(MAX_SPEED);   // задаем скорость движения
   motorFrontLeft.run(BACKWARD); // задаем движение назад
   motorFrontRight.run(BACKWARD); // задаем движение назад
-  motorFrontLeft.setSpeed(255);   // задаем скорость движения
-  motorFrontRight.setSpeed(255);   // задаем скорость движения
+  motorFrontLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
+  motorFrontRight.setSpeed(MAX_SPEED);   // задаем скорость движения
 
 
   delay(2000);          // указываем время движения
