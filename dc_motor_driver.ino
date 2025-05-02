59e7e73d4ef921e891f1d76a89e967179cfe6ca4
ilia <iliiliusiki@gmail.com>
2025-05-02 14:34:59 +0500

    go to speed variable

--- dc_motor_driver.ino
+++ dc_motor_driver.ino
@@ -1,10 +1,13 @@
 #include <AFMotor.h>     // подключаем библиотеку для шилда
 #define MAX_SPEED 255
+#define MIN_SPEED 70
 
 AF_DCMotor motorBackLeft(1); // подключаем мотор к клеммникам M1
 AF_DCMotor motorBackRight(2); // подключаем мотор к клеммникам M2
 AF_DCMotor motorFrontLeft(3); // подключаем мотор к клеммникам M3
 AF_DCMotor motorFrontRight(4); // подключаем мотор к клеммникам M4
+
+int speed = MIN_SPEED;
 
 void runMotor(AF_DCMotor motor, int speed, int direction) {
   motor.setSpeed(speed); // задаем максимальную скорость мотора
@@ -12,43 +15,20 @@ void runMotor(AF_DCMotor motor, int speed, int direction) {
 }
 
 void setup() {
-  runMotor(motorBackLeft, MAX_SPEED, RELEASE);
-  runMotor(motorBackRight, MAX_SPEED, RELEASE);
-  runMotor(motorFrontLeft, MAX_SPEED, RELEASE);
-  runMotor(motorFrontRight, MAX_SPEED, RELEASE);
+  Serial.begin(9600);
+  runMotor(motorBackLeft, speed, RELEASE);
+  runMotor(motorBackRight, speed, RELEASE);
+  runMotor(motorFrontLeft, speed, RELEASE);
+  runMotor(motorFrontRight, speed, RELEASE);
 }
 
 void loop() {
-  runMotor(motorBackLeft, MAX_SPEED, FORWARD);
-  runMotor(motorBackRight, MAX_SPEED, FORWARD);
-  runMotor(motorFrontLeft, MAX_SPEED, FORWARD);
-  runMotor(motorFrontRight, MAX_SPEED, FORWARD);
+  Serial.println(speed);
+  speed++; if (speed == MAX_SPEED) speed = MIN_SPEED;
+  runMotor(motorBackLeft, speed, FORWARD);
+  runMotor(motorBackRight, speed, FORWARD);
+  runMotor(motorFrontLeft, speed, FORWARD);
+  runMotor(motorFrontRight, speed, FORWARD);
 
-
-
-  delay(2000);          // указываем время движения
-
-  motorBackLeft.run(RELEASE);  // останавливаем мотор M1
-  motorBackRight.run(RELEASE);  // останавливаем мотор M2
-  motorFrontLeft.run(RELEASE);  // останавливаем мотор M1
-  motorFrontRight.run(RELEASE);  // останавливаем мотор M2
-
-  motorBackLeft.run(BACKWARD); // задаем движение назад
-  motorBackLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
-
-  motorBackRight.run(BACKWARD); // задаем движение назад
-  motorBackRight.setSpeed(MAX_SPEED);   // задаем скорость движения
-
-  motorFrontLeft.run(BACKWARD); // задаем движение назад
-  motorFrontLeft.setSpeed(MAX_SPEED);   // задаем скорость движения
-
-  motorFrontRight.run(BACKWARD); // задаем движение назад
-  motorFrontRight.setSpeed(MAX_SPEED);   // задаем скорость движения
-
-  delay(2000);          // указываем время движения
-
-  motorBackLeft.run(RELEASE);  // останавливаем мотор M1
-  motorBackRight.run(RELEASE);  // останавливаем мотор M2
-  motorFrontLeft.run(RELEASE);  // останавливаем мотор M1
-  motorFrontRight.run(RELEASE);  // останавливаем мотор M2
+  delay(100);          // указываем время движения
 }
