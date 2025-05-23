e5f78e9d5546c6c8d789c12ea0ed7a5d5229dce3
Ilia <iliiliusiki@gmail.com>
2025-05-23 10:20:38 +0500

    bug fixes

--- dc_motor_driver.ino
+++ dc_motor_driver.ino
@@ -1,13 +1,15 @@
 #include <AFMotor.h>     // подключаем библиотеку для шилда
 #define MAX_SPEED 255
-#define MIN_SPEED 70
+#define MIN_SPEED 100
 
 AF_DCMotor motorBackLeft(1); // подключаем мотор к клеммникам M1
 AF_DCMotor motorBackRight(2); // подключаем мотор к клеммникам M2
-AF_DCMotor motorFrontLeft(3); // подключаем мотор к клеммникам M3
-AF_DCMotor motorFrontRight(4); // подключаем мотор к клеммникам M4
+AF_DCMotor motorFrontLeft(4); // подключаем мотор к клеммникам M4
+AF_DCMotor motorFrontRight(3); // подключаем мотор к клеммникам M3
 
 int speed = MIN_SPEED;
+float startTime = 0.0;
+float deltaSpeed = 0.0;
 
 void runMotor(AF_DCMotor motor, int speed, int direction) {
   motor.setSpeed(speed); // задаем максимальную скорость мотора
@@ -16,6 +18,7 @@ void runMotor(AF_DCMotor motor, int speed, int direction) {
 
 void setup() {
   Serial.begin(9600);
+  startTime = micros();
   runMotor(motorBackLeft, speed, RELEASE);
   runMotor(motorBackRight, speed, RELEASE);
   runMotor(motorFrontLeft, speed, RELEASE);
@@ -24,7 +27,8 @@ void setup() {
 
 void loop() {
   Serial.println(speed);
-  speed++; if (speed == MAX_SPEED) speed = MIN_SPEED;
+  // Serial.println((micros() - startTime) / 10000.0, 2);
+  speed += deltaSpeed; if (speed == MAX_SPEED) speed = MIN_SPEED;
   runMotor(motorBackLeft, speed, FORWARD);
   runMotor(motorBackRight, speed, FORWARD);
   runMotor(motorFrontLeft, speed, FORWARD);
