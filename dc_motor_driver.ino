334ea566d4ce06222fd70a503d33c40d90b9bc56
Ilia <iliiliusiki@gmail.com>
2025-05-23 10:44:21 +0500

    implement runCar function

--- dc_motor_driver.ino
+++ dc_motor_driver.ino
@@ -1,38 +1,40 @@
-#include <AFMotor.h>     // подключаем библиотеку для шилда
+#include <AFMotor.h>
 #define MAX_SPEED 255
 #define MIN_SPEED 100
 
-AF_DCMotor motorBackLeft(1); // подключаем мотор к клеммникам M1
-AF_DCMotor motorBackRight(2); // подключаем мотор к клеммникам M2
-AF_DCMotor motorFrontLeft(4); // подключаем мотор к клеммникам M4
-AF_DCMotor motorFrontRight(3); // подключаем мотор к клеммникам M3
+AF_DCMotor motorBackLeft(1); // connect motor to connector M1
+AF_DCMotor motorBackRight(2); // connect motor to connector M2
+AF_DCMotor motorFrontLeft(4); // connect motor to connector M4
+AF_DCMotor motorFrontRight(3); // connect motor to connector M3
 
 int speed = MIN_SPEED;
 float startTime = 0.0;
 float deltaSpeed = 0.0;
 
 void runMotor(AF_DCMotor motor, int speed, int direction) {
-  motor.setSpeed(speed); // задаем максимальную скорость мотора
-  motor.run(direction);   // останавливаем мотор
+  motor.setSpeed(speed);
+  motor.run(direction);
+}
+
+void runCar(int speed, int direction, int angle) {
+  // angle will change from -100 to 100, negative means turn left
+  runMotor(motorBackLeft, speed, direction);
+  runMotor(motorBackRight, speed, direction);
+  runMotor(motorFrontLeft, speed, direction);
+  runMotor(motorFrontRight, speed, direction);
 }
 
 void setup() {
   Serial.begin(9600);
   startTime = micros();
-  runMotor(motorBackLeft, speed, RELEASE);
-  runMotor(motorBackRight, speed, RELEASE);
-  runMotor(motorFrontLeft, speed, RELEASE);
-  runMotor(motorFrontRight, speed, RELEASE);
+  runCar(speed, RELEASE, 0);
 }
 
 void loop() {
   Serial.println(speed);
   // Serial.println((micros() - startTime) / 10000.0, 2);
   speed += deltaSpeed; if (speed == MAX_SPEED) speed = MIN_SPEED;
-  runMotor(motorBackLeft, speed, FORWARD);
-  runMotor(motorBackRight, speed, FORWARD);
-  runMotor(motorFrontLeft, speed, FORWARD);
-  runMotor(motorFrontRight, speed, FORWARD);
+  runCar(speed, FORWARD, 0);
 
-  delay(100);          // указываем время движения
+  delay(100);
 }
